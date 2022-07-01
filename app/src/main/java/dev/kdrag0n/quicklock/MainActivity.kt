c01f58f0700c1d9fc130ffe0de20e9f79d6612ab
Danny Lin <danny@kdrag0n.dev>
2022-07-01 14:34:54 -0700

    Add ViewModel + Dagger/Hilt, update Compose

--- app/src/main/java/dev/kdrag0n/quicklock/MainActivity.kt
+++ app/src/main/java/dev/kdrag0n/quicklock/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.kdrag0n.quicklock.ui.theme.QuickLockTheme
+import dev.kdrag0n.quicklock.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QuickLockTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,7 +37,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    QuickLockTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
