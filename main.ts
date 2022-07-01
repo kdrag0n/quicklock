4ddb7f39cfa03250029a21d46b800fd3c0f2d4f8
Danny Lin <danny@kdrag0n.dev>
2022-07-01 15:15:58 -0700

    Switch to 2 spaces

--- main.ts
+++ main.ts
@@ -3,10 +3,10 @@ import fastify from 'fastify'
 const server = fastify({ logger: true })
 
 server.get('/', async (request, reply) => {
-    return { hello: 'world' }
+  return { hello: 'world' }
 })
 
 async function start() {
-    await server.listen({ port: 3002 })
+  await server.listen({ port: 3002 })
 }
 start()
