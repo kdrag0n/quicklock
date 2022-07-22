98db3f83f6652c087a9ce107e8947b489e6cfc3e
Danny Lin <danny@kdrag0n.dev>
2022-07-21 20:08:36 -0700

    scripts: Sync

--- web/scripts/build_larch.sh
+++ web/scripts/build_larch.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 emcmake cmake . \
-    -DCMAKE_PREFIX_PATH=/home/dragon/code/crypto/prefix-emptool \
+    -DCMAKE_PREFIX_PATH=/home/dragon/code/crypto/prefix-emptool-noexceptions \
     -DCMAKE_BUILD_TYPE=Release \
     -DOPENSSL_ROOT_DIR=/home/dragon/code/crypto/prefix-openssl-threads \
     -DOPENSSL_INCLUDE_DIR=/home/dragon/code/crypto/prefix-openssl-threads/include \
     -DOPENSSL_CRYPTO_LIBRARY=/home/dragon/code/crypto/prefix-openssl-threads/lib/libcrypto.a \
     -DOPENSSL_SSL_LIBRARY=/home/dragon/code/crypto/prefix-openssl-threads/lib/libssl.a \
-    -DCMAKE_FOLDER=/home/dragon/code/crypto/prefix-emptool \
-    -Demp-tool_DIR=/home/dragon/code/crypto/prefix-emptool/cmake \
-    -DEMP-TOOL_INCLUDE_DIR=/home/dragon/code/crypto/prefix-emptool/include \
-    -DEMP-TOOL_LIBRARY=/home/dragon/code/crypto/prefix-emptool/lib/libemp-tool.so
+    -DCMAKE_FOLDER=/home/dragon/code/crypto/prefix-emptool-noexceptions \
+    -Demp-tool_DIR=/home/dragon/code/crypto/prefix-emptool-noexceptions/cmake \
+    -DEMP-TOOL_INCLUDE_DIR=/home/dragon/code/crypto/prefix-emptool-noexceptions/include \
+    -DEMP-TOOL_LIBRARY=/home/dragon/code/crypto/prefix-emptool-noexceptions/lib/libemp-tool.so
 emmake make -j48 client_lib
 
     # -DCMAKE_C_FLAGS="-O3 -sASYNCIFY -sPROXY_TO_PTHREAD -sWASM_BIGINT -msimd128 -msse -msse2 -msse3 -mssse3 -msse4.1 -msse4.2 -mfpu=neon" \
